2a6616f9473050a3345d815ffe4a683eb848ec0a
Senin Adın <senin-mail@adresin.com>
2026-06-05 19:42:05 +0300

    fix: remove extra parenthesis in DashboardPage

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -192,8 +192,6 @@ export function DashboardPage() {
         </div>
       )}
 
-      )}
-
       {/* Grid containing Quick Actions and Chat */}
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         <div className="lg:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-4 h-min">
